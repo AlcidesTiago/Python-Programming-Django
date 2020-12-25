aaa0f8793e7b5e6c1c4b2576e317749ab77e5da7
AlcidesTiago <alcidesft6@gmail.com>
2020-12-26 02:00:19 +0530

    Working in Modules

---
+++ OOPS/Modules.py
@@ -0,0 +1,7 @@
+'''
+Modules refer to a file containing Python statements and definitions.
+
+A file containing Python code, for example: example.py, is called a module, and its module name would be example.
+
+We use modules to break down large programs into small manageable and organized files.
+'''
